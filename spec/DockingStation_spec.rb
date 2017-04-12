5754a62ff9aaf12d2080c240840ada974e123d57
milesillsley <milesillsley@hotmail.co.uk>
2017-04-12 18:32:23 +0100

    add test to recieve a default capacity

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,18 +3,27 @@ require 'DockingStation'
 
 describe DockingStation do
 
+  describe "set cappacity" do
+    it { is_expected.to receive(:new).with(Integer) }
+  end
+
+
   describe '#release' do
+
     it "raises error when there are no bikes to be released" do
       expect { subject.release }.to raise_error "No bikes to release"
     end
     it "a) gets a bike, and then b) expects the bike to be working" do
-		bike = Bike.new
-		subject.bikes << bike
-		expect(subject.release.working?).to eq(true)
-	end
+		  bike = Bike.new
+		  subject.bikes << bike
+		  expect(subject.release.working?).to eq(true)
+	  end
+
   end
 
+
   describe "#dock" do
+
     bike = Bike.new
     it "docks a bike at a docking station" do
       expect(subject.dock(bike)).to eq([bike])
@@ -22,12 +31,10 @@ describe DockingStation do
 
     it "won't accept more bikes than station's maximum capacity which is 20 bikes" do
     	MAX_CAPACITY.times{ bike = Bike.new
-    		subject.bikes << bike }
+    	subject.bikes << bike }
     	expect{subject.dock(bike)}.to raise_error "No more space"
-	end
+	  end
 
   end
-
-
 
 end
